f58b9e0f952767971db14a5e21d3ba9850d11e2c
briancaffey <briancaffey2010@gmail.com>
2023-01-08 17:18:35 -0500

    feat(gha): add gha workflow for publishing tagged version to npm

--- index.ts
+++ index.ts
@@ -1,2 +1,5 @@
 export * from "./src/components/ad-hoc/base";
 export * from "./src/components/ad-hoc/app";
+
+export * from "./src/components/prod/base";
+export * from "./src/components/prod/app";
